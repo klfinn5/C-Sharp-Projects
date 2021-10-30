0a004ede43475a28a11c744baaf9e5abca895b9c
Kelsey Finn <klfinn5@gmail.com>
2021-10-30 02:48:21 -0400

    csharptutortial

---
+++ Basic_C#_Programs/github_assignment.cs/github_assignment.cs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace github_accounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            string name = Console.ReadLine();
+            Console.WriteLine("Remember, we're accounting on you!");
+            Console.Read();
+        }
+    }
+}
