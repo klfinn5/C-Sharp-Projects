a3fd06fea0ea772ef32439b164e58954fc17d135
Kelsey Finn <klfinn5@gmail.com>
2021-11-01 19:24:07 -0400

    program

---
+++ Basic_C#_Programs/TA_DR.cs/TA_DR.cs/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TA_DR.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //greeting
+            Console.WriteLine("The Tech Academy");
+            Console.ReadLine();
+
+            //report
+            Console.WriteLine("Student Daily Report");
+            Console.ReadLine();
+
+            //name
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+            Console.ReadLine();
+
+            //course
+            Console.WriteLine("What course are you on?");
+            string yourCourse = Console.ReadLine();
+            Console.ReadLine();
+
+            //page
+            Console.WriteLine("What page are you on?");
+            string pgNum = Console.ReadLine();
+            Console.ReadLine();
+
+            //help
+            Console.WriteLine("Do you need help with anything? Please answer true or false.");
+            bool itsTrue = true;
+            if (itsTrue)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+            Console.ReadLine();
+
+            //experience
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            Console.ReadLine();
+
+            //feedback
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            Console.ReadLine();
+
+            //hours
+            Console.WriteLine("How many hours did you study today?");
+            string yourHours = Console.ReadLine();
+            Console.ReadLine();
+
+            //print
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/VariablesAndDataTypes.cs/VariablesAndDataTypes.cs/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a simple program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number is: " + favoriteNumber);
+            //Console.ReadLine();
+
+            //bool isStudying = false;
+            //byte hoursWorked = 42;
+            //sbyte currentTemperature = -23;
+            //char questionMark = "\u2013";
+
+            decimal_moneyInTheBank = 100.5m;
+
+            double heightInCentimeters = 34.33434334;
+
+            float secondsLeft = 3.32f;
+
+            short temperatureOnMars = -341;
+
+            string myName = "Kelsey";
+
+            int currentAge = 39;
+            string yearsOld = currentAge.ToString();
+
+            bool itsRaining = true;
+            string rainingStatus = Convert.ToString(itsRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+        }
+    }
+}
