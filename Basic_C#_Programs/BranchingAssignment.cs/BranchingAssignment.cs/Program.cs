87e6417a15afc2aa6ae35bc5befd452423223c1d
Kelsey Finn <klfinn5@gmail.com>
2021-11-03 17:05:08 -0400

    edit

--- Basic_C#_Programs/BranchingAssignment.cs/BranchingAssignment.cs/Program.cs
+++ Basic_C#_Programs/BranchingAssignment.cs/BranchingAssignment.cs/Program.cs
@@ -24,29 +24,25 @@ namespace BranchingAssignment.cs
             {
                 Console.WriteLine("Please enter the package width:");
                 int width = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package height:");
+                int height = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package length:");
+                int length = Convert.ToInt32(Console.ReadLine());
+
+                int dimension = (weight + height + length);
+                int total = ((weight * height * length * weight) / 100);
+
+                Console.WriteLine("Your estimated total for shipping this package is: $" + total + ". Thank you!");
+
             }
-            Console.ReadLine();
-
-            Console.WriteLine("Please enter the package height:");
-            int height = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Please enter the package length:");
-            int length = Convert.ToInt32(Console.ReadLine());
-
-            int dimension = (weight + height + length);
-            int total = ((weight * height * length * weight) / 100);
-             
-
-            if ((dimension) > 50)
+            else if (dimension > 50)
             {
                 Console.WriteLine("Package too big to be shipped via Package Express.");
             }
             Console.ReadLine();
-            
-            Console.WriteLine("Your estimated total for shipping this package is: $" + total + ". Thank you!");
-            Console.ReadLine();
-
-
+         
 
         }
     }
---
+++ Basic_C#_Programs/LoopsSwitch.cs/LoopsSwitch.cs/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsSwitch.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+
+            // while loop
+            //keep it running
+            //switch statement
+            //switch statement takes a input and checks it against a variety of values and performs the action called on the value it matches
+            //do ahead of while allows loop to run without might not run
+
+            do
+            {
+                switch (number) //what you want to check
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62. Try again.");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again.");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again.");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed 12. Correct!");
+                        isGuessed = true;
+                        break;
+                    default: //if none of the cases evaluated are true
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (isGuessed);
+            Console.Read();
+
+
+
+        }
+    }
+}
