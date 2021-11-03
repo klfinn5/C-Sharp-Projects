ddee0b08a7d3fe2373391c3967412fc7bc0e324f
Kelsey Finn <klfinn5@gmail.com>
2021-11-03 17:51:30 -0400

    loop

---
+++ Basic_C#_Programs/ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many cups in a gallon?");
+            int cups = Convert.ToInt32(Console.ReadLine());
+            bool answer = cups == 16; //correct answer
+
+            do //actually perform loop
+            {
+                switch (cups) //used to check values
+                {
+                    case 16:
+                        Console.WriteLine("Correct! :)");
+                        answer = true; //flagging so it knows its correct
+                        break;
+                    default:
+                        Console.WriteLine("Sorry, try again :(");
+                        Console.WriteLine("How many cups in a gallon?"); //will restate question
+                        cups = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (answer); //keep loop going can make it actually keep going *forever*
+            Console.Read();
+        }
+    }
+}
