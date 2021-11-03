eef79713f621c5cb5a85381bc62d7df38ff136eb
Kelsey Finn <klfinn5@gmail.com>
2021-11-03 18:48:31 -0400

    ewok

--- Basic_C#_Programs/ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
+++ Basic_C#_Programs/ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -10,6 +10,27 @@ namespace ConsoleAppAssignment
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("How many Ewoks live on Endor?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool ewok = number == 30000000;
+            while (ewok)
+            {
+                switch (number) //used to check values
+                {
+                    case 30000000:
+                        Console.WriteLine("You are correct!");
+                        ewok = true; //flagging so it knows its correct
+                        break;
+                    default:
+                        Console.WriteLine("Sorry, try again :(");
+                        Console.WriteLine("How many Ewoks live on Endor?"); //will restate question
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+
+
             Console.WriteLine("How many cups in a gallon?");
             int cups = Convert.ToInt32(Console.ReadLine());
             bool answer = cups == 16; //correct answer
@@ -31,6 +52,8 @@ namespace ConsoleAppAssignment
             }
             while (answer); //keep loop going can make it actually keep going *forever*
             Console.Read();
+
+
         }
     }
 }
---
+++ Basic_C#_Programs/Strings/Strings/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //a string is a data type
+
+            string name = "Kelsey";
+            //backslash \ means its still part of the string 
+            string quote = "She said, \"Tha's what\", Kelsey. \n Hello new line. \n \t Hello on tab";
+            string filename = @"C:\\Users\\Kelsey"; //file names need double
+
+            Console.WriteLine(quote);
+            Console.ReadLine();
+
+            bool trueOrFalse = name.Contains("s");
+            trueOrFalse = name.EndsWith("s");
+
+            int length = name.Length;
+
+            name = name.ToLower();
+            name = name.ToUpper();
+
+
+            string name = "Kelsey";
+            name = "Richard";
+
+            Console.WriteLine(name);
+            Console.ReadLine();
+
+
+
+
+            StringBuilder sb = new StringBuilder();
+            //dynamic object is expandable without loss of performance
+
+            sb.Append("My name is Kelsey");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
