4f1c27a20237236cd70aa1cd13365236634009b9
Kelsey Finn <klfinn5@gmail.com>
2021-12-23 11:10:51 -0500

    void

---
+++ Basic_C#_Programs/VoidAssignment/VoidAssignment/Empty.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidAssignment
+{
+    class Empty
+    {
+        public int yellow { get; } 
+        public int blue { get; }
+
+        public void ocean(out int yellow, out int blue) //out passes numbers
+        {
+            yellow = 20 * 2; //math operation
+            blue = 50;
+
+            //display 2nd number
+            Console.WriteLine(blue);
+
+            //specifying parameters by name
+            Console.WriteLine("In ocean, parameter value of blue = {0}", blue);
+            Console.WriteLine("In ocean, parameter value of yellow = {0}", yellow);
+            return;
+        }
+    }
+}
---
+++ Basic_C#_Programs/VoidAssignment/VoidAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Empty clear = new Empty();  //instantiate
+
+            //declare variables
+            int yellow;
+            int blue;
+            clear.ocean(out yellow, out blue); //passing numbers
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
