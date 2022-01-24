2c7d4e65c389f5eb2684327e0bad61a824d2c572
Kelsey Finn <klfinn5@gmail.com>
2022-01-23 21:05:17 -0500

    update

---
+++ Basic_C#_Programs/ComboMethodAssignment/ComboMethodAssignment/Classy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComboMethodAssignment
+{
+    public class Classy
+    {
+        public int meh { get; }
+
+        public void Style(out int meh)
+        {
+            int result;
+            result = meh / 2;
+            return;
+        }
+
+        public int Yes()
+        {
+            return meh * meh;
+        }
+
+
+    }
+}
---
+++ Basic_C#_Programs/ComboMethodAssignment/ComboMethodAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComboMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Classy pizzaz = new Classy();
+
+            Console.WriteLine("Pick a number.");
+            int user = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine(pizzaz.Style(out user));
+
+            Console.ReadLine();
+        }
+    }
+}
